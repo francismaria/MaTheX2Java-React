1c6d162e52e8637d940462f9dc20487e29d57e83
Francisco Maria <franciscomaria97@hotmail.com>
2022-03-13 22:48:33 +0000

    Add development tools (ESLint, Prettier, Husky)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import NotFound from './components/error/NotFound';
 
 function App() {
   return (
-    <div className="Root">
+    <section id="root-app">
       <Routes>
         <Route path="/" element={<Header />} />
-        <Route
-          path="*"
-          element={<NotFound />}
-        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </section>
   );
 }
 

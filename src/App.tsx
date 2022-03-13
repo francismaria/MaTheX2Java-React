9552f371fdfbe60d3df2447006b89b7c6b04eaf7
Francisco Maria <franciscomaria97@hotmail.com>
2022-03-13 20:43:50 +0000

    Add basic routing feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import NotFound from './components/error/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Root">
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
     </div>
   );
 }

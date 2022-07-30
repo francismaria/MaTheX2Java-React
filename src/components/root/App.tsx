e51ef792255477e83fa1faa76ea0245035e2ea61
Francisco Maria <franciscomaria97@hotmail.com>
2022-07-30 13:46:51 +0100

    Implement about panel

--- src/components/root/App.tsx
+++ src/components/root/App.tsx
@@ -5,6 +5,7 @@ import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import Translator from '../translator/Translator';
 import Rules from 'components/rules/Rules';
+import About from 'components/about/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Translator />} />
+        <Route path="/about" element={<About />} />
         <Route path="/rules" element={<Rules />} />
         <Route path="*" element={<NotFound />} />
       </Routes>

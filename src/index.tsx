d6cf41540f6de54e9ecbc7dd974f819d6501c98f
Francisco Maria <franciscomaria97@hotmail.com>
2024-06-02 11:03:53 +0200

    Update browser router for successful application navigation

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MaTheX2Java-React">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
